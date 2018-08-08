92f0aab7898b387bdcd479e16e73d19b1987ed19
William Lathrop <wlathrop10@gmail.com>
2018-08-08 20:23:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,7 +44,6 @@ def turn_count(board)
 end
 
 def current_player(board)
-  #if the turn count is an even number, that means O just went, so the next/current player is X
   num_turns = turn_count(board)
   if num_turns % 2 == 0
     player = "X"
